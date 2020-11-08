d345d8f10211b1c6f563a9db86050c3d0a57b65e
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 11:42:12 +0530

    exporting miltiple thing from a file not file

--- people.js
+++ people.js
@@ -3,7 +3,7 @@
 const people = ['Hello', 'Namaste', 'Swagat', 'Abhinandan' ,'Salam'];
 const age = [20,30,40];
 
-//export  multiple thing like people and age
+//export  multiple things like people and age
 module.exports ={
     
 people,age
