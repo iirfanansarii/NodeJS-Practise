dab2f86433e6015e9b65478612ee87bb75526a54
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 20:03:51 +0530

    the request object

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-/* here  we are going to create server using node js  
-*where we will see how to get a request from browser 
-* Not how to respond to the browser
-*/
+//import http
+const http = require("http");
 
-/*http module
-*Purpose:The first thing require to create a server is http module
-*/
-
-const http = require('http');
-
-/* createServer method
-*Purpose:method to create server
-*/
-
-/*req & res
-*Purpose:Request from client side like get,post,delete,patch..etc for client side 
-*res: Response from server side 
-*/
-
-//This code runs in backend i.e in termimal
-const server = http.createServer((req,res) => {
-   console.log('request made');
+//create server
+const server = http.createServer((req, res) => {
+  console.log(req.url,req.method);
 });
 
-// listening to loacal server 
-server.listen(3000,'localhost',() =>{
-    console.log('Listening for request on  port 3000')
-})
+// listen to server
+server.listen(3000, "localhost", () => {
+  console.log("Listening for request on  port 3000");
+});
+
