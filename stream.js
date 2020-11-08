40b5f402cdcccfaf6fbb37b451741473bb1f93e4
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 16:14:23 +0530

    write stream

--- stream.js
+++ stream.js
@@ -10,12 +10,20 @@
  * 
   */
 
+/* createWriteStream
+* Purpose:To write one stream data into another stream
+*Process:When readStream creates it takes data from read stream and then it goes to writeStream 
+*We haven't created the blog.txt file when writeStream file execute it create that file we have just given name
+ */
+
+
   /*on
   *Method Type: On Click Event 
    */
 
 /* Note:
 * we can make any stream in readable form either using toString method or encoding method
+* when writeStream execute it create new file which we have defined in it's argument
 */
 
 
@@ -24,10 +32,14 @@ const fs = require('fs');
 
 
 const readStream = fs.createReadStream('./docs/blog.txt',{encoding:'utf-8'});
+const writeStream = fs.createWriteStream('./docs/blog2.txt')
 
 readStream.on('data',(chunk) => {
 
     console.log('-------------New Chunk----------');
     console.log(chunk);
+
+    writeStream.write("\n New Chunk \n");
+    writeStream.write(chunk);
 
 })
