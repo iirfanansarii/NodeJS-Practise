296d1f73590732c880a3904c2929dab1cb1b8fd2
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 15:17:22 +0530

    delete files

--- file.js
+++ file.js
@@ -3,58 +3,30 @@
  *Module Type: Inbuilt node module
  */
 
-/* Directories-1
- *Purpose:To create new directory or folder
- *Method : mkdir
- *Method Type:asyc
- *Argument:It rquire two argument one is directory name which is about to created
- * and second one is callback function
+/*Purpose:delete a file
+ *Method:unlink
+ *Argument:two argument require in which one is location of the file and another one is callback function
  */
 
- /* Directories-2
- *Purpose:To remove existing directory or folder
- *Method : rmdir
- *Method Type:asyc
- *Argument:It rquire two argument one is directory name which is about to removed
- * and second one is callback function
+/* existsSync()
+ *Purpose:block the further code if it already exists
+ *Argument:require only one argument
  */
 
-
-/* existsSync() method
- *This method require one parameter and if that exists then it blocks further code
- *that is why we have used Not(!)
- */
-
-//import inbuilt file system(fs) node module
+//import inbuilt file system node module
 const fs = require("fs");
 
-//creating directory
-
-if (!fs.existsSync("./assets")) {
-  //directories
-  fs.mkdir("./assets", (err) => {
+//delelting files
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
     if (err) {
       console.log(err);
     }
-    console.log("folder created");
+    console.log("file deleted");
   });
- 
 }
-else{
-    fs.rmdir('./assets',(err) =>{
-      if(err){
-          console.log(err);
-      }
-      console.log('Folder removed ')
-    })
-}
-
-
 
 /* Note:
- *Since this method use to create new diectory or folder so when you'd run this code
- *at the very firs time it will create that folder but if you'd run the same code
- * it wil show you error that the directory is already created .
- * so better change the name of directory when you run again run same code
- * or apply exitsSync() method to block the further code if that folder/directory already exists
- *  */
+ * This method would delete the file only when it exits that mean if you'd run thid code
+ * again and agian this is not goig to show you any error
+ */
