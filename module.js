8545ef2d3269e6bad2705d5e07b9b92e3d35be0c
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 11:53:49 +0530

    importing multiple thing

--- module.js
+++ module.js
@@ -5,9 +5,9 @@
 
 
 /* Importing only people data from file people.js */
-const {people} = require("./people");
+const {people,age} = require("./people");
 
 
 /* since only importing people data from people file so can only display people data */
-console.log(people);
+console.log(people,age);
 
