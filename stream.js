02f9f5cc7c20e746f4ab313e2d4a99ff2a8422f7
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 16:25:19 +0530

    pipe

--- stream.js
+++ stream.js
@@ -17,29 +17,18 @@
  */
 
 
-  /*on
-  *Method Type: On Click Event 
-   */
-
-/* Note:
-* we can make any stream in readable form either using toString method or encoding method
-* when writeStream execute it create new file which we have defined in it's argument
-*/
-
+ /*Piping
+ *Purpose:Take the data from read stream and pipe it to the write stream 
+ * It is a easy way to create write stream 
+ */
 
 //import inbuilt file system node module
 const fs = require('fs');
 
 
 const readStream = fs.createReadStream('./docs/blog.txt',{encoding:'utf-8'});
-const writeStream = fs.createWriteStream('./docs/blog2.txt')
+const writeStream = fs.createWriteStream('./docs/blog3.txt');
 
-readStream.on('data',(chunk) => {
+//piping
 
-    console.log('-------------New Chunk----------');
-    console.log(chunk);
-
-    writeStream.write("\n New Chunk \n");
-    writeStream.write(chunk);
-
-})
+readStream.pipe(writeStream);
