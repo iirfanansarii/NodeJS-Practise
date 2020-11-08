45613871ce795204aa23e9fc954b884e1f147003
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 11:25:29 +0530

    Moduel & Require

---
+++ module.js
@@ -0,0 +1,15 @@
+//To interact with web page we will use node js in backend
+// some time we break our code into multiple module to maintian and access easily
+
+/* Moduel & Require 
+* Module: to break code into small file
+*Require: To import any module or file
+* */
+
+/* 
+* Here we are importing sigle thing i.e people from people file(people.js )and storing it at variable xyz using require
+*/
+
+const xyz = require("./people");
+console.log(xyz);
+
---
+++ people.js
@@ -0,0 +1,7 @@
+
+//array to store people
+const people = ['Hello', 'Namaste', 'Swagat', 'Abhinandan' ,'Salam'];
+console.log(people); 
+
+//export is use export any file into another fiel
+module.exports = people;
