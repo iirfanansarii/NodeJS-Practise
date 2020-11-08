8c6be5df2f47aad1f5ca45aa01ab978661c627f0
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-08 10:50:47 +0530

    global object in node and window

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-//To get current directory of our file 
+/* 
+*Global object in node is different  from the global object in window(console)
+* so most of the thing in window object we can't access
+*eg:DOM method 
+* i.e console.log(document.querySelector);
+*you cant access this method coz document is defined in window object
+*/
 
-console.log(__dirname);
-console.log(__filename);
+console.log(document.querySelector);
+
+//error: ReferenceError: document is not defined
+//you cant access this method coz document is defined in window object
+
