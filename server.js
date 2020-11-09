b6f18f99afbe0a7c5a676fbff0333599c8ff83c4
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-09 10:24:23 +0530

    Basic routing in node using switch case

--- server.js
+++ server.js
@@ -11,17 +11,32 @@ const server = http.createServer((req, res) => {
  //set header content type
  res.setHeader('Content-Type','text/html');
 
- //send html file
- fs.readFile('./views/index.html',(err,data) => {
-     if(err){
-         console.log(err);
-         res.end();
-     }
-     else{
-        //  res.write(data);
-         res.end(data);
-     }
- })
+let path = './views/';
+switch (req.url) {
+  case "/":
+    path += "index.html";
+    break;
+
+  case "/about":
+    path += "about.html";
+    break;
+
+  default:
+    path += "404.html";
+    break;
+}
+
+
+ //send html file simple routing method
+ fs.readFile(path, (err, data) => {
+   if (err) {
+     console.log(err);
+     res.end();
+   } else {
+     //  res.write(data);
+     res.end(data);
+   }
+ });
 });
 
 // listen to server
